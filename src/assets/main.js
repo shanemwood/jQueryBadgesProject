ab5605d88554400e5fa20bc6a49914f28d803ef7
Shane Wood <shanemw@gmail.com>
2018-03-08 21:29:35 -0600

    completed tasks 5-9

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,26 @@
-$(function() {
+$(function(){
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/2263001.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      showBadges(response.courses.completed);
+    }
+  });
+
+  var showBadges = function(queryData) {
+
+    var badges = $('#badges');
+
+    queryData.forEach(function(course){
+      
+      var div = $('<div />', {'class': 'course'}).appendTo(badges);
+      $('<h3 />', {text: course.title}).appendTo(div);
+      $('<img />', {src: course.badge}).appendTo(div);
+      $('<a />', {href: course.url, target: '_blank', 'class':'btn btn-primary', text: 'See Course'}).appendTo(div);
+    
+    });
+
+  }
 
 });

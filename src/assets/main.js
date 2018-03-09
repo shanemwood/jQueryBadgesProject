d130d38bec13385a0b9389774bfb72d82992b734
Shane Wood <shanemw@gmail.com>
2018-03-08 22:04:21 -0600

    good stuff

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,38 +1,24 @@
-$(function() {
+$(function(){
 
   $.ajax({
     url: 'https://www.codeschool.com/users/2263001.json',
     dataType: 'jsonp',
-    success: function(data) {
-      populateWithCourses(data.courses.completed)
+    success: function(response) {
+      showBadges(response.courses.completed);
     }
   });
 
-  function populateWithCourses(courses) {
+  var showBadges = function(queryData) {
 
-    var $badges = $('#badges');
+    var badges = $('#badges');
 
-    courses.forEach(function(course) {
-
-      $div = $('<div />', {
-        'class': 'course'
-      }).appendTo($badges);
-
-      $('<h3 />', {
-        text: course.title
-      }).appendTo($div);
-
-      $('<img />', {
-        src: course.badge
-      }).appendTo($div);
-
-      $('<a />', {
-        'class': 'btn btn-primary',
-        target: '_blank',
-        href: course.url,
-        text: 'See Course'
-      }).appendTo($div);
-
+    queryData.forEach(function(course){
+      
+      var div = $('<div />', {'class': 'course'}).appendTo(badges);
+      $('<h3 />', {text: course.title}).appendTo(div);
+      $('<img />', {src: course.badge}).appendTo(div);
+      $('<a />', {href: course.url, target: '_blank', 'class':'btn btn-primary', text: 'See Course'}).appendTo(div);
+    
     });
 
   }
